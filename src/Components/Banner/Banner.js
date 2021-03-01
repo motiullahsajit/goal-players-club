2268c92986c64ad60421d3d6c7302d0b3f9bfc04
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-03-01 21:26:33 +0600

    last

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -3,7 +3,7 @@ import bannerImg from '../../images/banner.jpg'
 const Banner = () => {
     return (
         <div className="text-white text-center p-3" style={{ backgroundColor: '#000' }}>
-            <h1>Chose The Best of best Players For your Club</h1>
+            <h1>Select The Best of Best Players For Your Team</h1>
             <h2>Goal 50 2020: The best 50 players in the world</h2>
             <img className="w-100 my-1" src={bannerImg} alt="" />
             <h5>After a global vote involving 44 editions, Goal proudly presents its list of the best 25 male and best 25 female footballers on the planet for 2019-20</h5>
--- src/Components/PlayersCart/PlayersCart.js
+++ src/Components/PlayersCart/PlayersCart.js
@@ -5,15 +5,15 @@ const PlayersCart = (props) => {
     const totalSalary = cart.reduce((sum, player) => sum + player.salary, 0)
     return (
         <div style={{ backgroundColor: '#081F2C', color: '#fff' }} className="sticky-top p-3 mt-2">
-            <h1 className="text-center text-success">Your Club</h1>
-            <h3>Players Added: {cart.length}</h3>
+            <h1 className="text-center text-success">Your Team</h1>
+            <h3>Players Selected: {cart.length}</h3>
             <ul className="list-group my-4" style={{ color: '#081F2C' }}>
                 {
                     cart.map((player) => <li key={player.id} className="list-group-item fs-4">Name: {player.name} ; Salary: {player.salary}$</li>)
                 }
             </ul>
             <div className="d-grid">
-                <h5>Total Amount(Salary): {totalSalary} $</h5>
+                <h4>Total Amount(Salary): {totalSalary} $</h4>
                 <button className="btn btn-success">Confirm</button>
             </div>
 
